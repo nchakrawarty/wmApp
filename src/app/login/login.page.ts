4ed6c2edb6b91861b3702883f5dd30a9f5601a7c
Rachithshetty <shettyrachith04@gmail.com>
2024-02-16 11:13:37 +0530

    updated login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -14,14 +14,8 @@ export class LoginPage {
   constructor(private authService: AuthService, private router: Router) {}
 
   login() {
-    // Perform authentication logic here
-    if (this.authService.authenticate()) {
-      // Authentication successful, navigate to home page
+    
+      // Directly navigate to the home page without any authentication logic
       this.router.navigateByUrl('/home');
-    } else {
-      // Authentication failed, display error message
-      console.log('Authentication failed');
-      // display error message to the user using toast
     }
-  }
 }
