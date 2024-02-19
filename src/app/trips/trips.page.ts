bca012ed470c49835b5035537b940c3dc088eec6
Varun Gonsalves <gonsalvesvarun@gmail.com>
2024-02-19 18:40:32 +0530

    trips ui completed

--- src/app/trips/trips.page.ts
+++ src/app/trips/trips.page.ts
@@ -12,4 +12,29 @@ export class TripsPage implements OnInit {
   ngOnInit() {
   }
 
+  endTrip(id_trip_div: string) {
+    document.getElementById(id_trip_div)?.setAttribute("hidden","true");
+  }
+
+  expand(id_item: string, id_expand: string, id_icon: string) {
+    let item = document.getElementById(id_item);
+    let expand = document.getElementById(id_expand);
+    let icon = document.getElementById(id_icon);
+    if(item?.getAttribute("lines") === "none") {
+      item?.setAttribute("lines","inset");
+    } else {
+      item?.setAttribute("lines","none");
+    }
+    if(expand?.hasAttribute("hidden")) {
+      expand?.removeAttribute("hidden");
+    } else {
+      expand?.setAttribute("hidden","true");
+    }
+    if(icon?.getAttribute("name") === "chevron-down-outline") {
+      icon?.setAttribute("name","chevron-up-outline");
+    } else {
+      icon?.setAttribute("name","chevron-down-outline");
+    }
+  }
+
 }
