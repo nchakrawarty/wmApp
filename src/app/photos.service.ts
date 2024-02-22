486a65178c330b1b033e77602335685175f9bbe3
poojasshetty92 <poojasid92@gmail.com>
2024-02-22 12:17:13 +0530

    ok

---
+++ src/app/photos.service.ts
@@ -0,0 +1,18 @@
+// import { Injectable } from '@angular/core';
+//import { HttpClient } from '@angular/common/http';
+
+//@Injectable({
+  //providedIn: 'root'
+//})
+//export class ImageUploadService {
+
+  //constructor(private http: HttpClient) { }
+
+  //uploadImage(base64Data: string): Promise<any> {
+    //const url = " htt"
+    //const formData = new FormData();
+    //formData.append('file', base64Data);
+
+    //return this.http.post(url, formData).toPromise();
+  //}
+//}
